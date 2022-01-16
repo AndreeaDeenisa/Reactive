044449f1ba80c8156e1470aba1ec2d40161e01fd
unknown <stefparaschivescu@gmail.com>
2022-01-16 14:05:42 +0200

    modified Experiente to original state

--- app/routes/experienta.routes.js
+++ app/routes/experienta.routes.js
@@ -1,21 +1,25 @@
-module.exports = app => {
+module.exports = (app) => {
   const experiente = require("../controllers/experienta.controller.js");
 
+  var router = require("express").Router();
+
   // Creeaza o Experienta
-  app.post("/", experiente.creeazaExperienta);
+  router.post("/", experiente.creeazaExperienta);
 
   // Gaseste toate Experientele
-  app.get("/getExp", experiente.gasesteToateExperientele);
+  router.get("/", experiente.gasesteToateExperientele);
 
   // Modifica o Experienta in functie de ID
-  app.put("/id/:id", experiente.modificaExperienta);
+  router.put("/id/:id", experiente.modificaExperienta);
 
   // Sterge Experienta in functie de ID
-  app.delete("/id/:id", experiente.stergeExperienta);
+  router.delete("/id/:id", experiente.stergeExperienta);
 
   // Gaseste o singura Experienta in functie de ID
-  app.get("/id/:id", experiente.gasesteExperienta);
+  router.get("/id/:id", experiente.gasesteExperienta);
 
   // Gaseste experiente aplicand filtru dupa punct plecare si/sau punct sosire si/sau tipTransport
-  app.get("/filterBy", experiente.filterBy);
+  router.get("/filterBy", experiente.filterBy);
+
+  app.use("/experiente", router);
 };
