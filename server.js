05ae40b981532bbdc9d1d05e05adafb6e4c116d7
unknown <stefparaschivescu@gmail.com>
2022-01-23 14:57:22 +0200

    removed server corsOptions for deployment

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:3001"
-};
-
-app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({
-  extended: true
-}));
-
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const db = require("./app/models");
 console.log(db);
-db.mongoose.connect(db.url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("Connected to the database!");
-}).catch(err => {
-  console.log("Cannot connect to the database!", err);
-  process.exit();
-});
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 // simple route
 app.get("/", (req, res) => {
-  res.send('<h1>Welcome to Mijloace Transport App</h1>');
+  res.send("<h1>Welcome to Mijloace Transport App</h1>");
 });
 
 require("./app/routes/experienta.routes")(app);
